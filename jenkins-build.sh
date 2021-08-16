0cc5f152a84dcc66b41e6e6364616b7943ff5cb0
object87 <jgarcia@objective87.com>
2021-08-16 21:51:31 +0000

    build overwrite

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/var/jenkins_home/workspace/packer build /var/jenkins_home/workspace/vsphere-http-ubuntu2004/ubuntu-20.04.json
+/var/jenkins_home/workspace/packer build -force /var/jenkins_home/workspace/vsphere-http-ubuntu2004/ubuntu-20.04.json
